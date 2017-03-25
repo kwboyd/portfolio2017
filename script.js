70336c7fc45c846de701d1968d619b3102472c38
Kate Boyd <kwboyd95@gmail.com>
2017-03-25 01:17:32 -0400

    Fixed triangles.

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-var el = document.getElementById('triangle-box');
-var rect = el.getBoundingClientRect();
-var triangle1 = document.getElementById("first-top-triangle");
-var triangle2 = document.getElementById("triangletop");
-var offset1 = rect.top;
+ var works = document.getElementById('bio');
+var els = document.getElementsByClassName('triangle-change');
+console.log(els);
+// var worksTop = works.getBoundingClientRect();
+var offsetWorks = works.offsetTop;
+console.log(offsetWorks);
+var triangle1desktop = document.getElementById("first-top-triangle");
+var triangle2desktop = document.getElementById("triangletop");
+var triangle1mobile = document.getElementById("first-mobile-triangle");
+var triangle2mobile = document.getElementById("triangletopmobile");
+// var offset1 = rect.top;
 
 
 function runOnScroll() {
-  console.log(offset1);
   console.log(document.body.scrollTop);
-    if (document.body.scrollTop >= 10) {
-      triangle1.style.fill = "#61879E";
-      triangle2.style.fill= "#61879E";
-    } else if (document.body.scrollTop < 10){
-      triangle1.style.fill = "#000";
-      triangle2.style.fill= "#000";
+  for (var i=0; i<els.length; i++) {
+    if (document.body.scrollTop >= 10 && document.body.scrollTop <= offsetWorks) {
+      els[i].style.fill = "#61879E";
+    } else if (document.body.scrollTop > offsetWorks){
+      els[i].style.fill = "#47568B";
     }
+    else {
+      els[i].style.fill = "#000";
+    }
+  }
  };
 window.addEventListener("scroll", runOnScroll);
